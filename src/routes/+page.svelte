15400e7e648655a485f60b4ee557e69cf6ceae96
timii <tim-t@hotmail.de>
2023-12-10 13:43:50 +0100

    style: create main grid layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<main class="grid h-screen w-screen gap-1">
+	<div class="workbench border-2 row-span-2">workbench</div>
+	<div class="preview border-2 row-span-2 col-span-2">preview</div>
+	<div class="timeline border-2 col-span-3">timeline</div>
+</main>
