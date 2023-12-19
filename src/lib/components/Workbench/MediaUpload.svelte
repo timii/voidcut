1f96febdc456a49ed4ad01e7fb4818ccb7eb917c
timii <tim-t@hotmail.de>
2023-12-19 09:51:42 +0100

    fix: image path

--- src/lib/components/Workbench/MediaUpload.svelte
+++ src/lib/components/Workbench/MediaUpload.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import { saveFilesToStore } from '$lib/utils/utils';
+	import MediaUploadImage from '$lib/assets/media-upload.png';
 
 	function onMediaUpload(e: Event) {
 		console.log('media added:', e);
@@ -16,8 +17,7 @@
 </script>
 
 <div class="media-upload flex flex-col justify-center items-center h-full gap-8">
-	<span class="media-upload-image">image placeholder</span>
-	<img src="$lib/assets/media-upload.png" alt="Media Upload" />
+	<img src={MediaUploadImage} class="media-upload-image" alt="Media Upload" />
 	<div class="media-upload-info flex flex-col w-1/3 text-center">
 		<span>You have no media files added to this project. Upload media files to get started.</span>
 		<span class="text-gray-500 text-sm italic"
