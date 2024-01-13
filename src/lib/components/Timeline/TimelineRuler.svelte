638c160d504b6144f63bce90d87c7adccac4a727
timii <tim-t@hotmail.de>
2024-01-13 21:01:16 +0100

    feat: add mouse move over timeline for thumb

--- src/lib/components/Timeline/TimelineRuler.svelte
+++ src/lib/components/Timeline/TimelineRuler.svelte
@@ -1,15 +1,34 @@
 <script lang="ts">
-	import { maxPlaybackTime } from '../../../stores/store';
+	import { maxPlaybackTime, thumbOffset, currentThumbPosition } from '../../../stores/store';
 
 	let maxPlaybackTimeVal = $maxPlaybackTime;
 
 	// calculate number of seconds of max playback time
 	let amountOfTicks = maxPlaybackTimeVal / 1000;
 	console.log('maxPlaybackTimeVal:', maxPlaybackTimeVal);
+
+	// move thumb to current position if mouse is clicked over ruler
+	function moveThumb(e: MouseEvent) {
+		// only necessary for mouse move event to check if a mouse button is held down
+		if (e.buttons === 1) {
+			$currentThumbPosition = e.clientX - $thumbOffset;
+			console.log(
+				'moveThumb:',
+				$currentThumbPosition,
+				'e.clientX:',
+				e.clientX,
+				'$thumbOffset:',
+				$thumbOffset
+			);
+		}
+	}
 </script>
 
+<!-- svelte-ignore a11y-no-static-element-interactions -->
 <div
 	class="timeline-ruler sticky top-0 left-0 h-7 pl-5 bg-background-color border-ruler-color border-t-2 flex z-[1] cursor-pointer"
+	on:mousedown={moveThumb}
+	on:mousemove={moveThumb}
 >
 	{#each { length: $maxPlaybackTime / 1000 } as _, i}
 		<div class="timeline-ruler-block w-12 flex flex-col items-start">
--- src/lib/components/Timeline/TimelineThumb.svelte
+++ src/lib/components/Timeline/TimelineThumb.svelte
@@ -1,33 +1,44 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
+	import { currentThumbPosition, thumbOffset } from '../../../stores/store';
 
-	let thumbPositionAfter = 0;
+	let thumbPosition = $currentThumbPosition;
 	let thumbOffsetLeft = 0;
 	let thumbElementRef: HTMLElement;
 
 	onMount(() => {
 		// calculate left offset of thumb element
 		thumbOffsetLeft = thumbElementRef.offsetLeft + Math.round(thumbElementRef.offsetWidth / 2);
+		$thumbOffset = thumbOffsetLeft;
 	});
 
 	// handle dragging thumb
 	function dragElement(e: MouseEvent) {
 		// console.log('e:', e);
+
+		// only drag element if mouse is held down
 		if (e.buttons === 1) {
 			e.preventDefault();
-			console.log(
-				'dragElement start -> thumbPositionAfter:',
-				thumbPositionAfter,
-				'e.clientX:',
-				e.clientX,
-				'offsetLeft:',
-				thumbElementRef.offsetLeft,
-				'thumbOffsetLeft:',
-				thumbOffsetLeft
-			);
+			// console.log(
+			// 	'dragElement start -> thumbPositionAfter:',
+			// 	thumbPosition,
+			// 	'e.clientX:',
+			// 	e.clientX,
+			// 	'offsetLeft:',
+			// 	thumbElementRef.offsetLeft,
+			// 	'thumbOffsetLeft:',
+			// 	thumbOffsetLeft
+			// );
 
 			// calculate new position using the current mouse position - the left offset of the thumb element
-			thumbPositionAfter = e.clientX - thumbOffsetLeft;
+			const newPos = e.clientX - thumbOffsetLeft;
+
+			// avoid the thumb to be moved further left than the tracks
+			if (newPos >= 0) {
+				thumbPosition = newPos;
+				$currentThumbPosition = thumbPosition;
+				console.log('currentThumbPosition:', $currentThumbPosition);
+			}
 		}
 	}
 </script>
@@ -36,7 +47,7 @@
 <div
 	class="timeline-thumb w-[12px] h-[calc(100%+28px)] absolute ml-5 z-10 -left-[6px] -top-7 cursor-grab"
 	bind:this={thumbElementRef}
-	style="transform: translateX({thumbPositionAfter}px)"
+	style="transform: translateX({$currentThumbPosition}px)"
 	on:mousemove={dragElement}
 >
 	<div class="thumb-container w-full h-full flex flex-col items-center relative">
