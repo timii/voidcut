fe5e908dc7ea6d0d10b06a65a6e188a6df99a12b
timii <tim-t@hotmail.de>
2024-01-14 17:18:17 +0100

    refactor: implement separate component for preview control button

---
+++ src/lib/components/Preview/PreviewControlsButton.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export let icon: string = '';
+	export let alt: string = '';
+	export let onClickCallback = () => {};
+</script>
+
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div on:click={onClickCallback} class="cursor-pointer">
+	<img src={icon} {alt} />
+</div>
