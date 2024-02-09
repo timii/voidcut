03c3452fe1bc877fb64c423ede8177fdcad7392c
timii <tim-t@hotmail.de>
2024-02-09 19:56:28 +0100

    feat: add generic component for element in timeline

--- src/lib/components/Timeline/TimelineRow.svelte
+++ src/lib/components/Timeline/TimelineRow.svelte
@@ -1,8 +1,13 @@
 <script lang="ts">
 	import type { ITimelineTrack } from '$lib/interfaces/Timeline';
+	import TimelineRowElement from './TimelineRowElement.svelte';
 
 	export let track: ITimelineTrack;
 	console.log('TimelineRow -> track:', track);
 </script>
 
-<div class="bg-ruler-color h-[50px] w-[1200px] mr-5"></div>
+<div class="bg-ruler-color h-[50px] opacity-50 w-full mr-5 rounded">
+	{#each track.elements as element}
+		<TimelineRowElement {element}></TimelineRowElement>
+	{/each}
+</div>
---
+++ src/lib/components/Timeline/TimelineRowElement.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import type { ITimelineElement } from '$lib/interfaces/Timeline';
+	import { currentTimelineScale } from '../../../stores/store';
+
+	export let element: ITimelineElement;
+
+	$: elementWidth = (element.duration / 1000) * $currentTimelineScale;
+	console.log(
+		'TimelineRowElement -> element:',
+		element,
+		'width:',
+		elementWidth,
+		'duration:',
+		element.duration,
+		'scale:',
+		$currentTimelineScale,
+		'calc:',
+		(element.duration / 1000) * $currentTimelineScale
+	);
+</script>
+
+<div class="bg-red-500 h-[50px] mr-5 rounded" style="width: {elementWidth}px;"></div>
