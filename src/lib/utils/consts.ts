c276c0c9a63124f181485c0431f5be9a4abca721
timii <tim-t@hotmail.de>
2024-03-25 20:11:27 +0100

    feat: add consts for often used values

--- src/lib/utils/consts.ts
+++ src/lib/utils/consts.ts
@@ -1,4 +1,7 @@
 export const CONSTS = {
     playbackIntervalTimer: 50,
-    secondsMultiplier: 1000
+    secondsMultiplier: 1000,
+    resetDelay: 50,
+    mediaPoolTransferKey: 'media-data',
+    timelineElTransferKey: 'timeline-move-data'
 }
