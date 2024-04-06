16e3cbc81cb8790040f7d16334f9493173aeff60
timii <tim-t@hotmail.de>
2024-04-06 20:39:30 +0200

    feat: change timeline divider color on element hover

--- src/lib/components/Timeline/TimelineRowDivider.svelte
+++ src/lib/components/Timeline/TimelineRowDivider.svelte
@@ -1,15 +1,51 @@
 <script lang="ts">
 	import type { ITimelineDraggedElement } from '$lib/interfaces/Timeline';
+	import { onMount } from 'svelte';
 	import { draggedElement } from '../../../stores/store';
+	import colors from 'tailwindcss/colors';
 
 	let dividerRef: HTMLElement;
 
 	$: isElementHovered($draggedElement);
 
+	let tracksElBoundRect: DOMRect;
+	let dividerElBoundRect: DOMRect;
+	let offsetInParent: { left: number; top: number };
+	let elementOverDivider = false;
+
+	onMount(() => {
+		const tracksEl = document.getElementsByClassName('timeline-tracks')[0];
+		tracksElBoundRect = tracksEl.getBoundingClientRect();
+		dividerElBoundRect = dividerRef.getBoundingClientRect();
+
+		offsetInParent = {
+			left: dividerElBoundRect.left - tracksElBoundRect.left,
+			top: dividerElBoundRect.top - tracksElBoundRect.top
+		};
+	});
+
 	function isElementHovered(draggedEl: ITimelineDraggedElement | null) {
 		if (!draggedEl) return;
-		console.log('isElementHovered -> draggedEl', draggedEl);
+		const curYPos = draggedEl.top + draggedEl.clickedY;
+		elementOverDivider =
+			curYPos >= offsetInParent.top && curYPos <= offsetInParent.top + dividerElBoundRect.height;
+		console.log(
+			'isElementHovered -> draggedEl',
+			draggedEl,
+			'dividerBounds:',
+			dividerElBoundRect,
+			'offsetInParent:',
+			offsetInParent,
+			'curYPos:',
+			curYPos,
+			'elementOverDivider:',
+			elementOverDivider
+		);
 	}
 </script>
 
-<div class="track-divider w-full bg-slate-500 h-[4px] rounded-sm" bind:this={dividerRef}></div>
+<div
+	class="track-divider w-ful h-[4px] rounded-sm"
+	style="background-color: {elementOverDivider ? 'red' : colors.slate[500]}"
+	bind:this={dividerRef}
+></div>
