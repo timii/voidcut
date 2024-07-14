a60fdd64f133b1a5ff1a2a8918e59d96e4a2af39
timii <tim-t@hotmail.de>
2024-07-14 21:42:25 +0200

    feat: add shared button component

---
+++ src/lib/components/shared/Button.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	export let text: string = '';
+	export let onClickCallback = () => {};
+</script>
+
+<button
+	on:click={onClickCallback}
+	class="px-2 py-[2px] rounded cursor-pointer bg-background-color-lighter hover:bg-background-color-lightest"
+>
+	{text}
+</button>
