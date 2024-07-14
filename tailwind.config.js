4fd4431b8e2282c470db407c97025729821de447
timii <tim-t@hotmail.de>
2024-07-14 21:42:52 +0200

    style: refactor background color tints

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,9 @@ export default {
       colors: {
         "ruler-color": '#42424e',
         "background-color": '#151519',
-        "background-color-lighter": '#2c2c2f',
+        "background-color-light": '#2c2c2f',
+        "background-color-lighter": '#434346',
+        "background-color-lightest": '#5b5b5e',
         "text-color": '#c2c2c2'
       }
     },
