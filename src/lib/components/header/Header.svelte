242e18d1ceb87939dcf22488b9412e2d5eb5c794
timii <tim-t@hotmail.de>
2024-07-15 21:43:04 +0200

    feat: add header component

---
+++ src/lib/components/header/Header.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { callFfmpeg } from '$lib/utils/ffmpeg.utils';
+	import Button from '../shared/Button.svelte';
+</script>
+
+<div class="flex items-center justify-between px-4 py-1 header">
+	<!-- TODO: add name and logo -->
+	<div>Name and logo</div>
+	<Button text={'Export'} onClickCallback={callFfmpeg}></Button>
+</div>
