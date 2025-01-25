bf16fef843bdd5cbff01e5064e390b9edb0486bd
timii <tim-t@hotmail.de>
2025-01-25 21:23:39 +0100

    feat: add tooltip component

---
+++ src/lib/components/shared/Tooltip.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import type { ITooltipCoords } from '$lib/interfaces/Tooltip';
+	import { computeTooltipPosition } from '$lib/utils/tooltip.utils';
+
+	export let text = '';
+
+	let isVisible = false;
+	let coords: ITooltipCoords = {
+		bottom: 0,
+		top: 0,
+		right: 0,
+		left: 0
+	};
+	let containerRef: HTMLSpanElement | null = null;
+	let tooltipRef: HTMLDivElement | null = null;
+	let timer: number | null = null;
+
+	function mouseOver() {
+		coords = computeTooltipPosition(containerRef, tooltipRef, coords);
+		console.log('coords:', coords);
+
+		// only show tooltip after a delay
+		timer = setTimeout(() => (isVisible = true), 500);
+	}
+
+	function mouseLeave() {
+		if (timer) {
+			clearTimeout(timer);
+			timer = null;
+		}
+		isVisible = false;
+	}
+</script>
+
+<!-- wrapper element that handles the hover -->
+<span
+	bind:this={containerRef}
+	on:mouseenter={mouseOver}
+	on:mouseleave={mouseLeave}
+	class="tooltip-container"
+>
+	<slot />
+</span>
+<div
+	bind:this={tooltipRef}
+	class="tooltip"
+	class:show={isVisible}
+	style="bottom: auto; right: auto; left: {coords.left}px; top: {coords.top}px;"
+>
+	{text}
+</div>
+
+<style lang="postcss">
+	.tooltip {
+		background-color: theme(colors.background-color-lighter);
+		border-radius: 12px;
+		color: theme(colors.text-color-lighter);
+		opacity: 0;
+		font-family: 'Noto Sans';
+		font-size: 11px;
+		font-weight: 600;
+		line-height: 0.9rem;
+		padding: 4px 12px;
+		pointer-events: none;
+		position: absolute;
+		text-align: left;
+		visibility: hidden;
+		white-space: nowrap;
+		z-index: 100;
+	}
+
+	.tooltip.show {
+		opacity: 1;
+		visibility: visible;
+		white-space: nowrap;
+	}
+
+	.tooltip:after {
+		border: 6px solid theme(colors.background-color-lighter);
+		content: ' ';
+		position: absolute;
+	}
+
+	.tooltip {
+		left: 50%;
+		top: 0;
+		transform: translate(-50%, calc(-100% - 6px));
+	}
+
+	.tooltip:after {
+		border-color: theme(colors.background-color-lighter) transparent transparent transparent;
+		bottom: 0;
+		left: 50%;
+		transform: translate(-50%, 99%);
+	}
+</style>
---
+++ src/lib/interfaces/Tooltip.ts
@@ -0,0 +1,6 @@
+export interface ITooltipCoords {
+    bottom: number;
+    top: number;
+    right: number;
+    left: number;
+}
