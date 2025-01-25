f4446487d75156e95e3cc2bd01b3e512d5402648
timii <tim-t@hotmail.de>
2025-01-25 21:46:38 +0100

    feat: add tooltip to IconButton component

--- src/lib/components/shared/IconButton.svelte
+++ src/lib/components/shared/IconButton.svelte
@@ -1,15 +1,31 @@
 <script lang="ts">
+	import Tooltip from './Tooltip.svelte';
+
 	export let icon: string = '';
 	export let alt: string = '';
+	export let tooltipText: string = '';
 	export let size = 26;
 	export let disabled = false;
 	export let onClickCallback = (e: Event) => {};
 </script>
 
-<button
-	on:click={onClickCallback}
-	{disabled}
-	class="cursor-pointer rounded p-[4px] hover:bg-background-color-lighter disabled:opacity-50 disabled:pointer-events-none"
->
-	<img src={icon} {alt} width={size} />
-</button>
+<!-- only show a tooltip tooltipText is given -->
+{#if tooltipText}
+	<Tooltip text={tooltipText}>
+		<button
+			on:click={onClickCallback}
+			{disabled}
+			class="cursor-pointer rounded p-[4px] hover:bg-background-color-lighter disabled:opacity-50 disabled:pointer-events-none"
+		>
+			<img src={icon} {alt} width={size} />
+		</button>
+	</Tooltip>
+{:else}
+	<button
+		on:click={onClickCallback}
+		{disabled}
+		class="cursor-pointer rounded p-[4px] hover:bg-background-color-lighter disabled:opacity-50 disabled:pointer-events-none"
+	>
+		<img src={icon} {alt} width={size} />
+	</button>
+{/if}
