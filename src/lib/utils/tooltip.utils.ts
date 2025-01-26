d7afc09e9e08c0155d4d9c90c98d0215fd94a58e
timii <tim-t@hotmail.de>
2025-01-26 21:55:09 +0100

    feat: adjust horizontal tooltip position if it goes over screen edge

--- src/lib/utils/tooltip.utils.ts
+++ src/lib/utils/tooltip.utils.ts
@@ -11,9 +11,20 @@ export const getTooltipPosition = (
 
     // get the position of tooltip container in window 
     const containerRect = containerRef.getBoundingClientRect();
+    const tooltipRect = tooltipRef.getBoundingClientRect();
 
     coords.top = containerRect.top;
     coords.left = containerRect.left + containerRect.width / 2;
+
+    // calculate if element goes outside of window, we need to add half the tooltip width since coords.left is at the middle of the tooltip right now
+    const widthOutsideWindow = coords.left + (tooltipRect.width / 2) - window.innerWidth
+
+    // if the element clips out of the window, adjust the left offset
+    if (widthOutsideWindow > 0) {
+        // calculate how much the toolbox needs to be adjusted by, to not be cut off at the screen edge
+        coords.left = coords.left - widthOutsideWindow
+    }
+    console.log("window width:", window.innerWidth, "coords.left:", coords.left, "tooltipRect.width:", tooltipRect.width, "widthOutsideWindow:", widthOutsideWindow, "containerRect.width:", containerRect.width);
 
     return coords;
 }
