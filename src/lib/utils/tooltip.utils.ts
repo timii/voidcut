4e35d836ae23f535f61108976bfc2db22f42dbe0
timii <tim-t@hotmail.de>
2025-02-07 21:52:43 +0100

    fix: adjust tooltip and its arrow on left screen edge cutoff

--- src/lib/utils/tooltip.utils.ts
+++ src/lib/utils/tooltip.utils.ts
@@ -16,16 +16,27 @@ export const getTooltipPosition = (
     coords.top = containerRect.top;
     coords.left = containerRect.left + containerRect.width / 2;
 
-    // calculate if element goes outside of window, we need to add half the tooltip width since coords.left is at the middle of the tooltip right now
-    const widthOutsideWindow = coords.left + (tooltipRect.width / 2) - window.innerWidth
+    // calculate if element goes outside right side of window, we need to add half the tooltip width since coords.left is at the middle of the tooltip right now
+    const widthOutsideRightEdge = coords.left + (tooltipRect.width / 2) - window.innerWidth
 
-    // if the element clips out of the window, adjust the left offset
-    if (widthOutsideWindow > 0) {
-        // calculate how much the toolbox needs to be adjusted by, to not be cut off at the screen edge
-        coords.left = coords.left - widthOutsideWindow
+    // calculate if element goes outside left side of screen 
+    const widthOutsideLeftEdge = coords.left - (tooltipRect.width / 2)
+
+    // if the element clips out of the right side of the window
+    if (widthOutsideRightEdge > 0) {
+        // subtract the tooltip width thats cut off from the orginal left offset
+        coords.left = coords.left - widthOutsideRightEdge
 
         // update the width cutoff property to use it to offset the arrow position
-        coords.widthCutoff = widthOutsideWindow
+        coords.widthCutoff = widthOutsideRightEdge
+    }
+    // if the element clips out of the left side of the window
+    else if (widthOutsideLeftEdge < 0) {
+        // add the tooltip width thats cut off tot the original left offset
+        coords.left = coords.left + Math.abs(widthOutsideLeftEdge)
+
+        // update the width cutoff property to use it to offset the arrow position
+        coords.widthCutoff = (widthOutsideLeftEdge)
     }
 
     return coords;
