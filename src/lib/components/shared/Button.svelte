98f5b8d7229845db2f7a530704e512b7e3a56981
timii <tim-t@hotmail.de>
2025-02-20 21:43:37 +0100

    style: add disabled style to button

--- src/lib/components/shared/Button.svelte
+++ src/lib/components/shared/Button.svelte
@@ -8,7 +8,7 @@
 <button
 	on:click={onClickCallback}
 	{disabled}
-	class="button px-4 py-2 font-medium rounded-lg cursor-pointer flex items-center gap-2 bg-background-button hover:bg-background-button-hover text-text-button hover:text-text-button-hover"
+	class="button px-4 py-2 font-medium rounded-lg cursor-pointer flex items-center gap-2 bg-background-button hover:bg-background-button-hover text-text-button hover:text-text-button-hover disabled:opacity-50 disabled:pointer-events-none"
 >
 	{#if icon}
 		<img src={icon} alt="Button Icon" width="16" />
