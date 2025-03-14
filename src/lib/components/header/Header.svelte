4ad967123f4828d3212019e62161c9d56ff5556c
timii <tim-t@hotmail.de>
2025-03-14 21:12:04 +0100

    feat: add debug button for getting and clearing storage

--- src/lib/components/header/Header.svelte
+++ src/lib/components/header/Header.svelte
@@ -6,20 +6,30 @@
 	import AboutIcon from '$lib/assets/header/about.png';
 	import ExportIcon from '$lib/assets/header/export.png';
 	import IconButton from '../shared/IconButton.svelte';
-	import { updateBackup } from '$lib/utils/persistence.utils';
+	import { clear, getState, updateState } from '$lib/utils/persistence.utils';
 
-	async function onExportButtonClick(e: Event) {
+	async function openExportDialog() {
 		exportOverlayOpen.set(true);
 		await callFfmpeg();
 	}
 
-	// updates the current state in local storage, mainly used for testing
-	function onBackupClicked() {
-		updateBackup();
+	function openAboutDialog() {
+		aboutOverlayOpen.set(true);
 	}
 
-	function onAboutButtonClick(e: Event) {
-		aboutOverlayOpen.set(true);
+	// updates the current state in local storage, mainly used for testing
+	function saveCurrentState() {
+		updateState();
+	}
+
+	// get the current state from local storage, mainly used for testing
+	function getLastSavedState() {
+		getState();
+	}
+
+	// clear local storage, mainly used for testing
+	function clearStorage() {
+		clear();
 	}
 </script>
 
@@ -34,11 +44,13 @@
 
 	<div class="flex justify-center items-center gap-4">
 		<!-- TODO: hide for production -->
-		<Button text={'Backup'} onClickCallback={onBackupClicked}></Button>
-		<IconButton icon={AboutIcon} size={20} onClickCallback={onAboutButtonClick}></IconButton>
+		<Button text={'Save'} onClickCallback={saveCurrentState}></Button>
+		<Button text={'Get'} onClickCallback={getLastSavedState}></Button>
+		<Button text={'Clear'} onClickCallback={clearStorage}></Button>
+		<IconButton icon={AboutIcon} size={20} onClickCallback={openAboutDialog}></IconButton>
 		<Button
 			text={'Export'}
-			onClickCallback={onExportButtonClick}
+			onClickCallback={openExportDialog}
 			icon={ExportIcon}
 			disabled={$timelineTracks.length < 1}
 		></Button>
